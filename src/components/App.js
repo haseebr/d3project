76d7ec630ab622fc4721646a72c95d4ac4e8f2d6
Haseeb ur Rahman <me@haseeb.co>
2017-03-24 15:12:16 -0400

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ export default class App extends React.Component {
       'sixty': 3600000
     };
     var time = (new Date()).getTime();
-    this.gt = time - intervals.thirty;
+    this.gt = time - intervals.five;
     this.lt = time;
 
     this.state = {
